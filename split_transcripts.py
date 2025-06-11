0d8a9edd74bcaf5222a3c8ff4fe00b0b1b52a4be
blancanyelvesz <blanca.alogon@gmail.com>
2025-06-11 13:56:57 +0200

    Updated to extract only relevant subjects and split into groups

--- split_transcripts.py
+++ split_transcripts.py
@@ -1,23 +1,42 @@
 import os
 
 filename = 'all_transcripts_v2.txt'
-output_dir = 'transcripts1'
-os.makedirs(output_dir, exist_ok = True)
+controls_file = 'controls_ids.txt'
+patients_file = 'patients_ids.txt'
+controls_dir = 'transcripts_controls'
+patients_dir = 'transcripts_patients'
 
+os.makedirs(controls_dir, exist_ok=True)
+os.makedirs(patients_dir, exist_ok=True)
+
+with open(controls_file, 'r', encoding='utf-8') as file:
+    controls_ids = set(line.strip() for line in file if line.strip())
+with open(patients_file, 'r', encoding='utf-8') as file:
+    patients_ids = set(line.strip() for line in file if line.strip())
 
 recording = None
 subject_id = None
 current_lines = []
 subject_list = []
-saved_transcripts = []
+saved_controls = []
+saved_patients = []
+not_relevant = []
 
 def save_transcript(subject_id, recording, current_lines):
     if subject_id is not None and current_lines and recording == '1st':
+        if subject_id in controls_ids:
+            output_dir = controls_dir
+            saved_controls.append(subject_id)
+        elif subject_id in patients_ids:
+            output_dir = patients_dir
+            saved_patients.append(subject_id)
+        else:
+            not_relevant.append(subject_id)
+            return
         output_filename = os.path.join(output_dir, f'{subject_id}.txt')
         with open(output_filename, 'w', encoding='utf-8') as output_file:
-            output_file.writelines(current_lines)
-        print(f'Wrote {len(current_lines)} lines to {output_filename}')
-        saved_transcripts.append(subject_id)
+            output_file.writelines(current_lines[1:])
+        print(f'Wrote {len(current_lines[1:])} lines to {output_filename}')
 
 
 with open(filename, 'r', encoding='utf-8') as file:
@@ -35,8 +54,8 @@ with open(filename, 'r', encoding='utf-8') as file:
     save_transcript(subject_id, recording, current_lines)
         
     print(f'Processed {len(set(subject_list))} subjects in total')
-    #print(set(subject_list))
-    print(f'and saved {len(saved_transcripts)} transcripts for 1st recordings')
+    print(f'and saved {len(saved_controls)+len(saved_patients)} transcripts for 1st recordings')
+    print(f'of which {len(saved_controls)} were controls and {len(saved_patients)} were patients')
 
-    # subjects_with_no_1st_recording = set(subject_list) - set(saved_subjects)
-    # print(subjects_with_no_1st_recording)
+    print(f'{len(set(not_relevant))} transcripts were not relevant for this study')
+    print(f'and {len(set(subject_list))-(len(saved_controls)+len(saved_patients))} subjects did not have a 1st recording')
