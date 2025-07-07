3d0cfa5138a45e9a72fe8526fa75a5034c02bba1
blancanyelvesz <blanca.alogon@gmail.com>
2025-07-07 15:36:47 +0200

    Updated little details

--- perplexity_openllama.py
+++ perplexity_openllama.py
@@ -34,7 +34,7 @@ if device.type == "cpu":
 
 
 model_name = 'openlm-research/open_llama_3b_v2'
-safe_model_name = model_name.replace("/", "_")
+safe_model_name = model_name.split('/')[1]
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map = "auto", torch_dtype = torch.float32)
 model = model.to(device)
